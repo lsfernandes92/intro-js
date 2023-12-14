4935f67f18917cbd56ff4f079ac74c6f073ee79d
lsfernandes92 <lsfernandes92@gmail.com>
2023-12-14 17:35:12 -0300

    Add first lesson

--- myFirstApp.js
+++ myFirstApp.js
@@ -1,1 +1,7 @@
 console.log('Hello World!');
+
+const age = 31;
+console.log('I am ' + age +  ' years old!');
+
+const ageIn5Years = age + 5;
+console.log('In 5 year I will be with ' + ageIn5Years + ' years old.');

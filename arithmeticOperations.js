e7e684822772233129a0acd3883612bf523ad837
lsfernandes92 <lsfernandes92@gmail.com>
2023-12-14 19:14:37 -0300

    Add third lesson

---
+++ arithmeticOperations.js
@@ -0,0 +1,10 @@
+ console.log('# Arithmetic operations\n');
+
+ console.log('This is a sum between numbers');
+ console.log('(2 + 2) = ' + (2 + 2));
+
+ console.log('\nAs in traditional school mathematics, the multiplication is done first');
+ console.log('(10 + 8 * 2) = ' + (10 + 8 * 2));
+
+ console.log('\nWhen using parentheses, the operation inside the parentheses are computed first');
+ console.log('((10 + 8) * 2) = ' + ((10 + 8) * 2));

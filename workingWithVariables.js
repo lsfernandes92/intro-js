87a4d8eb7cfd4fd2996d9b8c796737a94574c4d9
lsfernandes92 <lsfernandes92@gmail.com>
2023-12-14 19:14:23 -0300

    Add second lesson

---
+++ workingWithVariables.js
@@ -0,0 +1,13 @@
+console.log('# Working with variables.\n');
+
+// This is one way(the wrong way) to declare a variable in JS
+name = 'Lucas';
+
+// This is another way
+const lastName = 'Fernandes';
+
+// And this is another way
+let age = 31;
+
+console.log(`My name is ${name} ${lastName}`);
+console.log(`And I am ${age} years old.`);
